97e4a19e68e9f54d1f5e34a5fec52e5f580c08f6
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-08-30 18:08:56 +0300

    fix linters

--- classes/student.rb
+++ classes/student.rb
@@ -3,6 +3,7 @@ require_relative 'classroom'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
